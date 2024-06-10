9641ad637f581e8dce014137bf7222488a7f2acf
Harsh Kumar <harshkumar92200@gmail.com>
2024-06-10 19:10:36 +0530

    Initial Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Layout from './components/Layout/Layout';
+
+function App() {
+
+
+  return (
+    <>
+      <Layout>
+        <div className="min-h-80 max-h-fit p-8">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ex rem tempora numquam ipsum eum error enim possimus ipsam? Eum, perspiciatis! Nulla ducimus, odio ea dolorem labore sapiente! Dicta in molestiae magnam tenetur ad, corporis nihil nisi modi fugiat vitae!
+        </div>
+      </Layout>
+    </>
+  )
+}
+
+export default App
